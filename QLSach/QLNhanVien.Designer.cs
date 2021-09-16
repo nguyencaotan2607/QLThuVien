6da742eb655e5cda37f1aedbd22bf0dad0390cdc
Phan Duy Quang <1751010116quang@ou.edu.vn>
2021-09-16 21:40:25 +0700

    Form Nhan Vien

---
+++ QLSach/QLNhanVien.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace QLSach
+{
+    partial class QLNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbGioitinh = new System.Windows.Forms.ComboBox();
+            this.cbManv = new System.Windows.Forms.ComboBox();
+            this.lbDiachi = new System.Windows.Forms.Label();
+            this.lbNgaylamviec = new System.Windows.Forms.Label();
+            this.lbNamsinh = new System.Windows.Forms.Label();
+            this.lbSDT = new System.Windows.Forms.Label();
+            this.lbGioitinh = new System.Windows.Forms.Label();
+            this.lbTennv = new System.Windows.Forms.Label();
+            this.lbManv = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtTennv = new System.Windows.Forms.TextBox();
+            this.txtDiachi = new System.Windows.Forms.TextBox();
+            this.btMoi = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.lbNhanvien = new System.Windows.Forms.Label();
+            this.dgNV = new System.Windows.Forms.DataGridView();
+            this.dtNgayLamViec = new System.Windows.Forms.DateTimePicker();
+            this.dtNamSinh = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgNV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbGioitinh
+            // 
+            this.cbGioitinh.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbGioitinh.FormattingEnabled = true;
+            this.cbGioitinh.Location = new System.Drawing.Point(384, 80);
+            this.cbGioitinh.Name = "cbGioitinh";
+            this.cbGioitinh.Size = new System.Drawing.Size(601, 27);
+            this.cbGioitinh.TabIndex = 35;
+            // 
+            // cbManv
+            // 
+            this.cbManv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbManv.FormattingEnabled = true;
+            this.cbManv.Location = new System.Drawing.Point(64, 48);
+            this.cbManv.Name = "cbManv";
+            this.cbManv.Size = new System.Drawing.Size(189, 27);
+            this.cbManv.TabIndex = 34;
+            // 
+            // lbDiachi
+            // 
+            this.lbDiachi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbDiachi.AutoSize = true;
+            this.lbDiachi.Location = new System.Drawing.Point(0, 0);
+            this.lbDiachi.Name = "lbDiachi";
+            this.lbDiachi.Size = new System.Drawing.Size(67, 21);
+            this.lbDiachi.TabIndex = 25;
+            this.lbDiachi.Text = "Địa chỉ:";
+            // 
+            // lbNgaylamviec
+            // 
+            this.lbNgaylamviec.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbNgaylamviec.AutoSize = true;
+            this.lbNgaylamviec.Location = new System.Drawing.Point(272, 120);
+            this.lbNgaylamviec.Name = "lbNgaylamviec";
+            this.lbNgaylamviec.Size = new System.Drawing.Size(119, 21);
+            this.lbNgaylamviec.TabIndex = 26;
+            this.lbNgaylamviec.Text = "Ngày làm việc:";
+            // 
+            // lbNamsinh
+            // 
+            this.lbNamsinh.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbNamsinh.AutoSize = true;
+            this.lbNamsinh.Location = new System.Drawing.Point(272, 56);
+            this.lbNamsinh.Name = "lbNamsinh";
+            this.lbNamsinh.Size = new System.Drawing.Size(85, 21);
+            this.lbNamsinh.TabIndex = 23;
+            this.lbNamsinh.Text = "Năm sinh:";
+            // 
+            // lbSDT
+            // 
+            this.lbSDT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbSDT.AutoSize = true;
+            this.lbSDT.Location = new System.Drawing.Point(0, 0);
+            this.lbSDT.Name = "lbSDT";
+            this.lbSDT.Size = new System.Drawing.Size(47, 21);
+            this.lbSDT.TabIndex = 24;
+            this.lbSDT.Text = "SĐT:";
+            // 
+            // lbGioitinh
+            // 
+            this.lbGioitinh.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbGioitinh.AutoSize = true;
+            this.lbGioitinh.Location = new System.Drawing.Point(272, 88);
+            this.lbGioitinh.Name = "lbGioitinh";
+            this.lbGioitinh.Size = new System.Drawing.Size(77, 21);
+            this.lbGioitinh.TabIndex = 27;
+            this.lbGioitinh.Text = "Giới tính:";
+            // 
+            // lbTennv
+            // 
+            this.lbTennv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTennv.AutoSize = true;
+            this.lbTennv.Location = new System.Drawing.Point(0, 0);
+            this.lbTennv.Name = "lbTennv";
+            this.lbTennv.Size = new System.Drawing.Size(70, 21);
+            this.lbTennv.TabIndex = 29;
+            this.lbTennv.Text = "Tên NV:";
+            // 
+            // lbManv
+            // 
+            this.lbManv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbManv.AutoSize = true;
+            this.lbManv.Location = new System.Drawing.Point(0, 0);
+            this.lbManv.Name = "lbManv";
+            this.lbManv.Size = new System.Drawing.Size(64, 21);
+            this.lbManv.TabIndex = 28;
+            this.lbManv.Text = "Mã NV:";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSDT.Location = new System.Drawing.Point(64, 112);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(189, 27);
+            this.txtSDT.TabIndex = 22;
+            // 
+            // txtTennv
+            // 
+            this.txtTennv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTennv.Location = new System.Drawing.Point(64, 80);
+            this.txtTennv.Name = "txtTennv";
+            this.txtTennv.Size = new System.Drawing.Size(189, 27);
+            this.txtTennv.TabIndex = 21;
+            // 
+            // txtDiachi
+            // 
+            this.txtDiachi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDiachi.Location = new System.Drawing.Point(64, 144);
+            this.txtDiachi.MaxLength = 4;
+            this.txtDiachi.Name = "txtDiachi";
+            this.txtDiachi.Size = new System.Drawing.Size(492, 27);
+            this.txtDiachi.TabIndex = 20;
+            // 
+            // btMoi
+            // 
+            this.btMoi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btMoi.Location = new System.Drawing.Point(64, 204);
+            this.btMoi.Name = "btMoi";
+            this.btMoi.Size = new System.Drawing.Size(184, 33);
+            this.btMoi.TabIndex = 33;
+            this.btMoi.Text = "&Mới";
+            this.btMoi.UseVisualStyleBackColor = true;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btXoa.Location = new System.Drawing.Point(681, 204);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(300, 33);
+            this.btXoa.TabIndex = 30;
+            this.btXoa.Text = "&Xóa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            // 
+            // btSua
+            // 
+            this.btSua.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSua.Location = new System.Drawing.Point(516, 204);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(95, 33);
+            this.btSua.TabIndex = 31;
+            this.btSua.Text = "&Sửa";
+            this.btSua.UseVisualStyleBackColor = true;
+            // 
+            // btThem
+            // 
+            this.btThem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btThem.Location = new System.Drawing.Point(347, 204);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(98, 33);
+            this.btThem.TabIndex = 32;
+            this.btThem.Text = "&Thêm";
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // lbNhanvien
+            // 
+            this.lbNhanvien.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbNhanvien.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbNhanvien.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbNhanvien.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.lbNhanvien.Location = new System.Drawing.Point(0, 0);
+            this.lbNhanvien.Name = "lbNhanvien";
+            this.lbNhanvien.Size = new System.Drawing.Size(993, 38);
+            this.lbNhanvien.TabIndex = 19;
+            this.lbNhanvien.Text = "QUẢN LÝ NHÂN VIÊN";
+            this.lbNhanvien.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgNV
+            // 
+            this.dgNV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgNV.Location = new System.Drawing.Point(4, 287);
+            this.dgNV.Name = "dgNV";
+            this.dgNV.RowHeadersWidth = 51;
+            this.dgNV.RowTemplate.Height = 24;
+            this.dgNV.Size = new System.Drawing.Size(977, 160);
+            this.dgNV.TabIndex = 38;
+            // 
+            // dtNgayLamViec
+            // 
+            this.dtNgayLamViec.Location = new System.Drawing.Point(384, 113);
+            this.dtNgayLamViec.Name = "dtNgayLamViec";
+            this.dtNgayLamViec.Size = new System.Drawing.Size(466, 27);
+            this.dtNgayLamViec.TabIndex = 39;
+            // 
+            // dtNamSinh
+            // 
+            this.dtNamSinh.Location = new System.Drawing.Point(384, 50);
+            this.dtNamSinh.Name = "dtNamSinh";
+            this.dtNamSinh.Size = new System.Drawing.Size(466, 27);
+            this.dtNamSinh.TabIndex = 40;
+            // 
+            // QLNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 462);
+            this.Controls.Add(this.dtNamSinh);
+            this.Controls.Add(this.dtNgayLamViec);
+            this.Controls.Add(this.dgNV);
+            this.Controls.Add(this.lbNhanvien);
+            this.Controls.Add(this.cbGioitinh);
+            this.Controls.Add(this.btThem);
+            this.Controls.Add(this.btSua);
+            this.Controls.Add(this.cbManv);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.btMoi);
+            this.Controls.Add(this.lbNgaylamviec);
+            this.Controls.Add(this.lbNamsinh);
+            this.Controls.Add(this.lbSDT);
+            this.Controls.Add(this.lbTennv);
+            this.Controls.Add(this.txtDiachi);
+            this.Controls.Add(this.txtTennv);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.lbManv);
+            this.Controls.Add(this.lbGioitinh);
+            this.Controls.Add(this.lbDiachi);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "QLNhanVien";
+            this.Text = "frmNhanvien";
+            ((System.ComponentModel.ISupportInitialize)(this.dgNV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cbGioitinh;
+        private System.Windows.Forms.ComboBox cbManv;
+        private System.Windows.Forms.Label lbDiachi;
+        private System.Windows.Forms.Label lbNgaylamviec;
+        private System.Windows.Forms.Label lbNamsinh;
+        private System.Windows.Forms.Label lbSDT;
+        private System.Windows.Forms.Label lbGioitinh;
+        private System.Windows.Forms.Label lbTennv;
+        private System.Windows.Forms.Label lbManv;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtTennv;
+        private System.Windows.Forms.TextBox txtDiachi;
+        private System.Windows.Forms.Button btMoi;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.Label lbNhanvien;
+        private System.Windows.Forms.DataGridView dgNV;
+        private System.Windows.Forms.DateTimePicker dtNgayLamViec;
+        private System.Windows.Forms.DateTimePicker dtNamSinh;
+    }
+}
